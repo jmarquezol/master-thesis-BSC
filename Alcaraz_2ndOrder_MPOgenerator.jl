cf452fb70689a206071501b2fa223375284b9859
jmarquezol <jmarquezolguin@gmail.com>
2026-04-30 00:07:31 +0200

    please help stefano

--- Alcaraz_2ndOrder_MPOgenerator.jl
+++ Alcaraz_2ndOrder_MPOgenerator.jl
@@ -86,17 +86,14 @@ function expH_alcaraz_2nd_opt(sites::Vector{<:Index}, lambda::Number, p::Number;
 
     # Using formulasfrom Appendix A of Van Damme et al. 2023:
     
-    # Macro-Row 1
     W11 = I_op .+ tau .* D .+ (tau^2 / 2) .* otimes(D, D) .+ (tau^3 / 6) .* otimes(D, otimes(D, D))
     W12 = C .+ (tau / 2) .* sym_sum([C, D]) .+ (tau^2 / 6) .* sym_sum([C, D, D])
     W13 = otimes(C, C) .+ (tau / 3) .* sym_sum([C, C, D])
 
-    # Macro-Row 2
     W21 = tau .* B .+ (tau^2 / 2) .* sym_sum([B, D]) .+ (tau^3 / 6) .* sym_sum([B, D, D])
     W22 = A .+ (tau / 2) .* (sym_sum([B, C]) .+ sym_sum([A, D])) .+ (tau^2 / 6) .* (sym_sum([C, B, D]) .+ sym_sum([A, D, D]))
     W23 = sym_sum([A, C]) .+ (tau / 3) .* (sym_sum([A, C, D]) .+ sym_sum([C, C, B]))
 
-    # Macro-Row 3
     W31 = (tau^2 / 2) .* otimes(B, B) .+ (tau^3 / 6) .* sym_sum([B, B, D])
     W32 = (tau / 2) .* sym_sum([A, B]) .+ (tau^2 / 6) .* (sym_sum([A, B, D]) .+ sym_sum([B, B, C]))
     W33 = otimes(A, A) .+ (tau / 3) .* (sym_sum([A, B, C]) .+ sym_sum([A, A, D]))
@@ -137,6 +134,7 @@ function expH_alcaraz_2nd_opt(sites::Vector{<:Index}, lambda::Number, p::Number;
     
     return U_dt
 end
+
 
 # 1ST ORDER MPO GENERATOR
 
@@ -281,6 +279,145 @@ function expH_alcaraz_WII(sites::Vector{<:Index}, lambda::Number, p::Number; dt:
             W = ITensor(ComplexF64, links[i-1], links[i], s', s)
             for row in 1:4, col in 1:4, s1 in 1:2, s2 in 1:2
                 W[links[i-1]=>row, links[i]=>col, s'=>s1, s=>s2] = W_II[row, col][s1, s2]
+            end
+            U_dt[i] = W
+        end
+    end
+    
+    return U_dt
+end
+
+using LinearAlgebra
+using ITensors
+
+function otimes(X::Matrix{Matrix{ComplexF64}}, Y::Matrix{Matrix{ComplexF64}})
+    nx, mx = size(X); ny, my = size(Y)
+    Z = Matrix{Matrix{ComplexF64}}(undef, nx * ny, mx * my)
+    for ix in 1:nx, jx in 1:mx
+        for iy in 1:ny, jy in 1:my
+            row = (ix - 1) * ny + iy
+            col = (jx - 1) * my + jy
+            Z[row, col] = X[ix, jx] * Y[iy, jy] 
+        end
+    end
+    return Z
+end
+
+function expH_alcaraz_2nd_opt_explicit(sites::Vector{<:Index}, lambda::Number, p::Number; dt::Number)
+    N = length(sites)
+    
+    I2 = ComplexF64[1 0; 0 1]
+    Z  = ComplexF64[1 0; 0 -1]
+    X  = ComplexF64[0 1; 1 0]
+    O2 = ComplexF64[0 0; 0 0]
+    
+    tau = -im * dt 
+
+    # DYNAMIC PRUNING: Keep this to protect the RTM eigensolver at p=0.0
+    if p == 0.0
+        D = Matrix{Matrix{ComplexF64}}(undef, 1, 1); D[1, 1] = -lambda .* X
+        C = Matrix{Matrix{ComplexF64}}(undef, 1, 1); C[1, 1] = Z
+        B = Matrix{Matrix{ComplexF64}}(undef, 1, 1); B[1, 1] = -Z
+        A = Matrix{Matrix{ComplexF64}}(undef, 1, 1); A[1, 1] = O2
+        dim_W = 1
+    else
+        D = Matrix{Matrix{ComplexF64}}(undef, 1, 1); D[1, 1] = -lambda .* X
+        C = Matrix{Matrix{ComplexF64}}(undef, 1, 3); C[1, 1] = Z; C[1, 2] = O2; C[1, 3] = X
+        B = Matrix{Matrix{ComplexF64}}(undef, 3, 1); B[1, 1] = -Z; B[2, 1] = -p .* Z; B[3, 1] = -p * lambda .* X
+        A = Matrix{Matrix{ComplexF64}}(undef, 3, 3)
+        for i in 1:3, j in 1:3 A[i, j] = O2 end
+        A[1, 2] = I2
+        dim_W = 3
+    end
+    
+    I_op = Matrix{Matrix{ComplexF64}}(undef, 1, 1); I_op[1, 1] = I2
+
+    # --- EXPLICIT COMBINATORIAL EXPANSION ---
+    
+    W11 = I_op .+ tau .* D .+ 
+          (tau^2 / 2) .* otimes(D, D) .+ 
+          (tau^3 / 6) .* otimes(D, otimes(D, D))
+
+    W12 = C .+ 
+          (tau / 2) .* (otimes(C, D) .+ otimes(D, C)) .+ 
+          (tau^2 / 6) .* (otimes(C, otimes(D, D)) .+ otimes(D, otimes(C, D)) .+ otimes(D, otimes(D, C)))
+
+    W13 = otimes(C, C) .+ 
+          (tau / 3) .* (otimes(C, otimes(C, D)) .+ otimes(C, otimes(D, C)) .+ otimes(D, otimes(C, C)))
+
+    W21 = tau .* B .+ 
+          (tau^2 / 2) .* (otimes(B, D) .+ otimes(D, B)) .+ 
+          (tau^3 / 6) .* (otimes(B, otimes(D, D)) .+ otimes(D, otimes(B, D)) .+ otimes(D, otimes(D, B)))
+
+    W22 = A .+ 
+          (tau / 2) .* (otimes(B, C) .+ otimes(C, B) .+ otimes(A, D) .+ otimes(D, A)) .+ 
+          (tau^2 / 6) .* (
+              otimes(C, otimes(B, D)) .+ otimes(C, otimes(D, B)) .+ 
+              otimes(B, otimes(C, D)) .+ otimes(B, otimes(D, C)) .+ 
+              otimes(D, otimes(C, B)) .+ otimes(D, otimes(B, C)) .+ 
+              otimes(A, otimes(D, D)) .+ otimes(D, otimes(A, D)) .+ otimes(D, otimes(D, A))
+          )
+
+    W23 = (otimes(A, C) .+ otimes(C, A)) .+ 
+          (tau / 3) .* (
+              otimes(A, otimes(C, D)) .+ otimes(A, otimes(D, C)) .+ 
+              otimes(C, otimes(A, D)) .+ otimes(C, otimes(D, A)) .+ 
+              otimes(D, otimes(A, C)) .+ otimes(D, otimes(C, A)) .+ 
+              otimes(C, otimes(C, B)) .+ otimes(C, otimes(B, C)) .+ otimes(B, otimes(C, C))
+          )
+
+    W31 = (tau^2 / 2) .* otimes(B, B) .+ 
+          (tau^3 / 6) .* (otimes(B, otimes(B, D)) .+ otimes(B, otimes(D, B)) .+ otimes(D, otimes(B, B)))
+
+    W32 = (tau / 2) .* (otimes(A, B) .+ otimes(B, A)) .+ 
+          (tau^2 / 6) .* (
+              otimes(A, otimes(B, D)) .+ otimes(A, otimes(D, B)) .+ 
+              otimes(B, otimes(A, D)) .+ otimes(B, otimes(D, A)) .+ 
+              otimes(D, otimes(A, B)) .+ otimes(D, otimes(B, A)) .+ 
+              otimes(B, otimes(B, C)) .+ otimes(B, otimes(C, B)) .+ otimes(C, otimes(B, B))
+          )
+
+    W33 = otimes(A, A) .+ 
+          (tau / 3) .* (
+              otimes(A, otimes(B, C)) .+ otimes(A, otimes(C, B)) .+ 
+              otimes(B, otimes(A, C)) .+ otimes(B, otimes(C, A)) .+ 
+              otimes(C, otimes(A, B)) .+ otimes(C, otimes(B, A)) .+ 
+              otimes(A, otimes(A, D)) .+ otimes(A, otimes(D, A)) .+ otimes(D, otimes(A, A))
+          )
+
+    # --- END EXPLICIT COMBINATORIAL EXPANSION ---
+
+    # Assemble full block matrix
+    W_opt = hvcat((3, 3, 3), W11, W12, W13, W21, W22, W23, W31, W32, W33)
+    
+    # Calculate exact dimension based on pruning (3 for p=0.0, 13 for p!=0.0)
+    dim_opt = 1 + dim_W + dim_W^2
+    
+    # Build ITensor MPO using strict OBC
+    links = [Index(dim_opt, "Link,l=$i") for i in 1:N-1]
+    U_dt = MPO(sites)
+    
+    for i in 1:N
+        s = sites[i]
+        
+        if i == 1
+            W = ITensor(ComplexF64, links[1], s', s)
+            for col in 1:dim_opt, s1 in 1:2, s2 in 1:2
+                W[links[1]=>col, s'=>s1, s=>s2] = W_opt[1, col][s1, s2]
+            end
+            U_dt[i] = W
+            
+        elseif i == N
+            W = ITensor(ComplexF64, links[i-1], s', s)
+            for row in 1:dim_opt, s1 in 1:2, s2 in 1:2
+                W[links[i-1]=>row, s'=>s1, s=>s2] = W_opt[row, 1][s1, s2]
+            end
+            U_dt[i] = W
+            
+        else
+            W = ITensor(ComplexF64, links[i-1], links[i], s', s)
+            for row in 1:dim_opt, col in 1:dim_opt, s1 in 1:2, s2 in 1:2
+                W[links[i-1]=>row, links[i]=>col, s'=>s1, s=>s2] = W_opt[row, col][s1, s2]
             end
             U_dt[i] = W
         end
